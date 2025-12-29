f34329fe4dac79bd187fcb6c6ad2261503d1c3d2
Rostik <rostislavdenysenko@gmail.com>
2025-12-29 03:13:50 +0200

    Added Hand class

--- BlackJack/Hand.cpp
+++ BlackJack/Hand.cpp
@@ -1,1 +1,46 @@
 #include "Hand.h"
+
+void Hand::addCard(std::unique_ptr<Card> card)
+{
+	mCards.push_back(std::move(card));
+}
+
+void Hand::clear()
+{
+	mCards.clear();
+}
+
+int Hand::getScore() const
+{
+	int score = 0;
+	int aceCount = 0;
+
+	for (const auto& card : mCards)
+	{
+		int value = card->getValue();
+		score += value;
+
+		if (card->getRank() == Rank::Ace)
+		{
+			aceCount++;
+		}
+	}
+
+	while (score > 21 && aceCount > 0)
+	{
+		score -= 10;
+		aceCount--;
+	}
+
+	return score;
+}
+
+size_t Hand::getCardCount() const
+{
+	return mCards.size();
+}
+
+const std::vector<std::unique_ptr<Card>>& Hand::getCards() const
+{
+	
+}
--- BlackJack/Hand.h
+++ BlackJack/Hand.h
@@ -1,5 +1,20 @@
 #pragma once
+#include <vector>
+#include <memory>
+
+#include "Card.h"
+
 class Hand
 {
+public:
+	void addCard(std::unique_ptr<Card> card);
+	void clear();
+	int getScore() const;
+	size_t getCardCount() const;
+
+	const std::vector<std::unique_ptr<Card>>& getCards() const;
+
+private:
+	std::vector<std::unique_ptr<Card>> mCards;
 };
 
